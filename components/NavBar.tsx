1f42114cf48534f4e6d925185ebb24a25ea59dfa
Vincent550102 <5020559@gmail.com>
2025-01-31 17:46:30 +0800

    Update GitHub text

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -67,7 +67,7 @@ export default function NavBar() {
 
   return (
     <nav className="bg-gray-800 p-4 text-white flex justify-between items-center fixed top-0 left-0 right-0 z-10">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-1">
         <div
           className="text-xl font-bold cursor-pointer"
           onClick={handleTitleClick}
@@ -81,7 +81,7 @@ export default function NavBar() {
           rel="noopener noreferrer"
           className="text-gray-400 hover:text-gray-300 transition-colors"
         >
-          GitHub
+          source code
         </a>
       </div>
       {selectedDomain && (
